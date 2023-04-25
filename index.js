6a20649b53258a24aa111da3553d2af31f9dc8c4
Futakuchi <8th.jpn@gmail.com>
2023-04-25 23:05:28 +0900

    sendとresultを追加

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const qs = require("qs");
+const request = require('request');
 const app = express();
 const port = process.env.PORT || 3001;
 
 const sleep = (m) => {
   return new Promise((resolve) => setTimeout(resolve, m));
 };
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/api/wait', async(req, res) => {
   const ret = {
@@ -20,6 +26,39 @@ app.get('/api/wait', async(req, res) => {
 
   res.json(ret);
 });
+
+app.get('/api/result', async(req, res) => {
+  const url = `https://${process.env.HOST}/services/apexrest/result`;
+  const sec = parseInt(process.env.WAIT || '0', 10);
+  await sleep(sec * 1000);
+
+  request.get({
+    uri: url,
+    qs: req.query
+  }, (err, req, data) => {
+    res.write(data);
+    res.end();
+  });
+});
+
+app.post('/api/send', async(req, res) => {
+  const url = `https://${process.env.HOST}/services/apexrest/send`;
+  const sec = parseInt((req.body.To || '00000000000')[8], 10) * 10;
+  process.env.WAIT = sec.toString();
+  await sleep(sec * 1000);
+
+  request.post({
+    uri: url,
+    headers: {
+      "content-type": "application/x-www-form-urlencoded; charset=utf-8"
+    },
+    body: qs.stringify(req.body)
+  }, (err1, res1, data1) => {
+    res.write(data1);
+    res.end();
+  });
+});
+
 
 // サーバ起動
 app.listen(port, () => console.log(`Listening on port ${port}`));
